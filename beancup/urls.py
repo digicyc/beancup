b9e75c4de4709de65c7a0d0879156314b7b106bd
AaronA <digicyc@gmail.com>
2014-02-18 10:35:28 -0700

    Connect login template to django auth.

--- beancup/urls.py
+++ beancup/urls.py
@@ -3,11 +3,13 @@ from django.contrib import admin
 
 admin.autodiscover()
 
-urlpatterns = patterns('beancup.views',
-    url(r'^$', 'home', name='home'),
+urlpatterns = patterns('',
+    url(r'^$', 'beancup.views.home', name='home'),
     url(r'^beanblog/', include('beanblog.urls')),
     url(r'^beanvote/', include('votebean.urls')),
     url(r'^admin/', include(admin.site.urls)),
-    url(r'^login/', 'bean_login', name='bean_login'),
-    url(r'^logout/', 'bean_logout', name='bean_logout'),
+    url(r'^login/', 'beancup.views.bean_login', name='bean_login'),
+    url(r'^logout/', 'beancup.views.bean_logout', name='bean_logout'),
+    url(r'^accounts/login/$', 'django.contrib.auth.views.login', {'template_name': 'auth/login.html'}),
+
 )
